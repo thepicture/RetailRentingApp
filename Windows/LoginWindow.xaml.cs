9c4b9b4e68370ecb58e3218570f59e7fb2c3b93e
thepicture <fuzzyeastern@protonmail.com>
2021-09-09 17:36:03 +0100

    Added new styles, LoginWindow is built, added functionality to it

--- Windows/LoginWindow.xaml.cs
+++ Windows/LoginWindow.xaml.cs
@@ -24,5 +24,62 @@ namespace RetailRentingApp
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Actions for login into the current app.
+        /// </summary>
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Actions for exiting the current app.
+        /// </summary>
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Точно покинуть приложение?", "Подтверждение выхода", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                App.Current.Shutdown();
+            }
+        }
+
+        private void CheckPasswordBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckPasswordBox.IsChecked == true)
+            {
+                HidePassword();
+                DisableLoginButton();
+            }
+            else
+            {
+                RevealPassword();
+                EnableLoginButton();
+            }
+        }
+
+        private void EnableLoginButton()
+        {
+            BtnLogin.IsEnabled = true;
+        }
+
+        private void DisableLoginButton()
+        {
+            BtnLogin.IsEnabled = false;
+        }
+
+        private void HidePassword()
+        {
+            TBoxPasswordInsecure.Text = PBoxPasswordSecure.Password;
+            PBoxPasswordSecure.Visibility = Visibility.Collapsed;
+            TBoxPasswordInsecure.Visibility = Visibility.Visible;
+        }
+
+        private void RevealPassword()
+        {
+            PBoxPasswordSecure.Password = TBoxPasswordInsecure.Text;
+            TBoxPasswordInsecure.Visibility = Visibility.Collapsed;
+            PBoxPasswordSecure.Visibility = Visibility.Visible;
+        }
     }
 }
