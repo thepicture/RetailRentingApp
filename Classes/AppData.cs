66bdb290a7614976cb3d6224897385e9797e2197
thepicture <fuzzyeastern@protonmail.com>
2021-09-09 17:49:16 +0100

    AppData class implemented

---
+++ Classes/AppData.cs
@@ -0,0 +1,13 @@
+﻿using RetailRentingApp.Backend;
+
+namespace RetailRentingApp.Classes
+{
+    /// <summary>
+    /// Application data which manages all of the backend context.
+    /// </summary>
+    class AppData
+    {
+        public static RetailRentingBaseEntities Context = new RetailRentingBaseEntities();
+        public static User CurrentUser = new User();
+    }
+}
