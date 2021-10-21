c626e5254f97f6154f1bf7a19444dd0a7a6025fe
thepicture <fuzzyeastern@protonmail.com>
2021-10-21 08:28:34 +0100

    Add trading location's page view

---
+++ Pages/TradingLocationsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RetailRentingApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for TradingLocationsPage.xaml
+    /// </summary>
+    public partial class TradingLocationsPage : Page
+    {
+        public TradingLocationsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnModifyTradingArea_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteTradingArea_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnClearFiltration_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
