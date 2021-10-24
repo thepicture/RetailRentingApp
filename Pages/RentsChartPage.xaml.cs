b7439a9edfae74fce79578dd3235da552ac06f06
thepicture <fuzzyeastern@protonmail.com>
2021-10-24 18:21:27 +0100

    Add functionality of RentsChartPage without Y values

--- Pages/RentsChartPage.xaml.cs
+++ Pages/RentsChartPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using RetailRentingApp.Backend;
+using RetailRentingApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace RetailRentingApp.Pages
 {
@@ -23,6 +16,46 @@ namespace RetailRentingApp.Pages
         public RentsChartPage()
         {
             InitializeComponent();
+            InitChart();
+            OverwhelmComboChartTypes();
+            UpdateChart();
+        }
+
+        private void OverwhelmComboChartTypes()
+        {
+            ComboChartType.ItemsSource = Enum.GetValues(typeof(SeriesChartType));
+        }
+
+        private void InitChart()
+        {
+            ChartRentsUsability.ChartAreas
+                            .Add(new ChartArea("RentsUsability"));
+
+            Series series = new Series("MainSeries")
+            {
+                IsValueShownAsLabel = true
+            };
+
+            ChartRentsUsability.Series.Add(series);
+        }
+
+        private void ChartType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateChart();
+        }
+
+        private void UpdateChart()
+        {
+            SeriesChartType chartType = (SeriesChartType)ComboChartType.SelectedItem;
+            Series chartSeries = ChartRentsUsability.Series.First();
+            chartSeries.Points.Clear();
+            chartSeries.ChartType = chartType;
+
+            List<RentingOfTradingArea> areas = AppData.Context.RentingOfTradingAreas.ToList();
+            foreach(RentingOfTradingArea area  in areas)
+            {
+                _ = chartSeries.Points.AddXY(area.Renting.Customer.CompanyName + ", " + area.TradingArea.Name, 20);
+            }
         }
     }
 }
